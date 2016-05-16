78a5f1b81338404d8801456ae345aac11423e3cc
omelhoro <igor.fischer154@gmail.com>
2016-05-16 11:58:03 +0200

    some cleanup

--- src/routes/ShoppingList/modules/redux.js
+++ src/routes/ShoppingList/modules/redux.js
@@ -17,31 +17,6 @@ const RESETSTATE = 'RESETSTATE';
 // ------------------------------------
 // Actions
 // ------------------------------------
-export function increment(value = 1) {
-  return {
-    type: COUNTER_INCREMENT,
-    payload: value,
-  };
-}
-
-/*  This is a thunk, meaning it is a function that immediately
-    returns a function for lazy evaluation. It is incredibly useful for
-    creating async actions, especially when combined with redux-thunk!
-
-    NOTE: This is solely for demonstration purposes. In a real application,
-    you'd probably want to dispatch an action of COUNTER_DOUBLE and let the
-    reducer take care of this logic.  */
-
-export const doubleAsync = () => {
-  return (dispatch, getState) => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        dispatch(increment(getState().counter));
-        resolve();
-      }, 200);
-    });
-  };
-};
 
 export function addToCard(e) {
   return {
@@ -82,7 +57,7 @@ export function addDevFromName(dev) {
     const out = await fetch(`/getdev?dev=${dev}`);
     const outJson = await out.json();
     dispatch(addToDevList(outJson));
-  };
+  }
 }
 
 function setToken(token) {
@@ -185,8 +160,6 @@ export function useCoupon(event) {
 }
 
 export const actions = {
-  increment,
-  doubleAsync,
   addToCard,
   removeFromCard,
   addDevFromOrg,
@@ -197,7 +170,6 @@ export const actions = {
   endOrdering,
   loadStateFromToken,
 };
-
 
 // ------------------------------------
 // Action Handlers
