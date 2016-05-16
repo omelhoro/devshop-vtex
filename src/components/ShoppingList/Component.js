cbeb229dca03c17a7985233d128429c37abca2cd
omelhoro <igor.fischer154@gmail.com>
2016-05-17 00:32:13 +0200

    implemented pagination

--- src/components/ShoppingList/Component.js
+++ src/components/ShoppingList/Component.js
@@ -7,9 +7,11 @@ const listElement = (ctx) => (
     <div className="media">
       <div className="media-left media-middle">
         <a href="#">
-          <img src={ctx.e.avatar_url} alt={ctx.e.login} style={{
-            maxWidth: 100
-          }}/>
+          <img
+            src={ctx.e.avatar_url} alt={ctx.e.login} style={{
+              maxWidth: 100,
+            }}
+          />
         </a>
       </div>
       <div className="media-body">
@@ -43,29 +45,34 @@ const listElement = (ctx) => (
             <tr>
               <td>Ordered hours</td>
               <td>
-                <input className="form-control hours" value={ctx.e.appAdded.orderedHours} disabled={ctx.e.isInCard} onChange={ctx
+                <input
+                  className="form-control hours" value={ctx.e.appAdded.orderedHours}
+                  disabled={ctx.e.isInCard} onChange={ctx
                   .calculatePrice
-                  .bind(null, ctx.e)} type="number"/>
+                  .bind(null, ctx.e)} type="number"
+                />
               </td>
             </tr>
           </tbody>
         </table>
 
         <div className="">
-          <button disabled={!ctx.e.appAdded.orderedHours} style={{
-            display: ctx.e.isInCard
-              ? 'none'
-              : 'inherit'
-          }} className="btn btn-block btn-success add-to-card" onClick={ctx
+          <button
+            disabled={!ctx.e.appAdded.orderedHours} style={{
+              display: ctx.e.isInCard
+                ? 'none'
+                : 'inherit'
+            }} className="btn btn-block btn-success add-to-card" onClick={ctx
             .addToCard
             .bind(null, ctx.e)}>
             Add {ctx.e.login} to card for {ctx.e.appAdded.totalSum}$
           </button>
-          <button className="btn btn-block btn-warning" style={{
-            display: ctx.e.isInCard
-              ? 'inherit'
-              : 'none'
-          }} onClick={ctx
+          <button
+            className="btn btn-block btn-warning" style={{
+              display: ctx.e.isInCard
+                ? 'inherit'
+                : 'none'
+            }} onClick={ctx
             .removeFromCard
             .bind(null, ctx.e)}>Remove from card
           </button>
@@ -92,7 +99,33 @@ const priceFormatWithDiscount = ({sumOriginal, discount, sum}) => (
     <h4 style={{display: discount ? 'inherit' : 'none'}}>{sumOriginal}$ - {discount}%</h4>
     <h2><span id="total-sum">{sum}</span>$</h2>
   </div>
-)
+);
+
+const renderPageLink = (ctx, i) => (
+  <li onClick={ctx.changePage.bind(null, i)}
+    className={i === ctx.currentPage ? 'active' : ''}><a href="#">{i + 1}</a></li>
+);
+
+const pagination = (props) => (
+  <ul
+    className="pagination"
+    style={{
+      display: props.pages > 1 ? 'flex' : 'none',
+    }}
+  >
+    <li>
+      <a href="#" aria-label="Previous" onClick={() => props.changePage(props.currentPage - 1)}>
+        <span aria-hidden="true">&laquo;</span>
+      </a>
+    </li>
+    {_.range(props.pages).map(renderPageLink.bind(null, props))}
+    <li>
+      <a href="#" aria-label="Next" onClick={() => props.changePage(props.currentPage + 1)}>
+        <span aria-hidden="true">&raquo;</span>
+      </a>
+    </li>
+  </ul>
+);
 
 export const ShoppingList = (props) => (
   <div>
@@ -103,19 +136,26 @@ export const ShoppingList = (props) => (
       </span>
     </h2>
 
-    <div className="row" style={{
-      margin: 10
-    }}>
+    <div
+      className="row" style={{
+        margin: 10,
+      }}
+    >
       <div className="col-sm-7">
         <div className="input-group">
-          <span className="input-group-addon" style={{
-            minWidth: 120
-          }}>
+          <span
+            className="input-group-addon" style={{
+              minWidth: 120,
+            }}
+          >
             User
           </span>
-          <input id="dev-name" className="form-control" type="text" placeholder="e.g. omelhoro"/>
+          <input id="dev-name" className="form-control" type="text" placeholder="e.g. omelhoro" />
           <span className="input-group-btn">
-            <button id="import-developer" className="btn btn-default" type="button" onClick={() => props.addDevFromName(dv('#dev-name'))}>
+            <button
+              id="import-developer" className="btn btn-default" type="button"
+              onClick={() => props.addDevFromName(dv('#dev-name'))}
+            >
               Import!
             </button>
           </span>
@@ -123,19 +163,26 @@ export const ShoppingList = (props) => (
       </div>
     </div>
 
-    <div className="row" style={{
-      margin: 10
-    }}>
+    <div
+      className="row" style={{
+        margin: 10,
+      }}
+    >
       <div className="col-sm-7">
         <div className="input-group">
-          <span className="input-group-addon" style={{
-            minWidth: 120
-          }}>
+          <span
+            className="input-group-addon" style={{
+              minWidth: 120,
+            }}
+          >
             Organization
           </span>
-          <input id="org-name" className="form-control" type="text" placeholder="e.g. Homebrew"/>
+          <input id="org-name" className="form-control" type="text" placeholder="e.g. Homebrew" />
           <span className="input-group-btn">
-            <button id="import-organization" className="btn btn-default" type="button" onClick={() => props.addDevFromOrg(dv('#org-name'))}>
+            <button
+              id="import-organization" className="btn btn-default"
+              type="button" onClick={() => props.addDevFromOrg(dv('#org-name'))}
+            >
               Import!
             </button>
           </span>
@@ -143,10 +190,13 @@ export const ShoppingList = (props) => (
       </div>
     </div>
 
+
+    {pagination(props)}
     <div className="row">
       <div id="developers-list" className="col-sm-8">
         {props
           .developers
+          .slice(props.currentPage * props.devsOnPage, (props.currentPage + 1) * props.devsOnPage)
           .map(e => listElement({
             e,
             ...props,
@@ -154,37 +204,56 @@ export const ShoppingList = (props) => (
       </div>
 
       <div className="col-sm-4">
-        <ul className="list-group">
-          {props
-            .shoppingcard
-            .map(e => listShoppingCard({
-              e,
-              ...props,
-            }))}
-        </ul>
-
-        <div style={{
-          display: props.shoppingcard.length
-            ? 'inherit'
-            : 'none'
-        }}
+        <div
+          className="fixed" style={{
+            position: 'fixed',
+            margin: '10px',
+          }}
         >
+          <ul className="list-group">
+            {props
+              .shoppingcard
+              .map(e => listShoppingCard({
+                e,
+                ...props,
+              }))}
+          </ul>
 
-        <div className="input-group">
-          <span className="input-group-addon">Coupon</span>
-          <input id="coupon-entry" placeholder="SHIPIT" value={props.coupon} onChange={props.useCoupon} className="form-control" type="text" />
-        </div>
+          <div
+            style={{
+              display: props.shoppingcard.length
+              ? 'inherit'
+              : 'none',
+            }}
+          >
+            <div className="input-group">
+              <span className="input-group-addon">Coupon</span>
+              <input
+                id="coupon-entry"
+                placeholder="SHIPIT" value={props.coupon}
+                onChange={props.useCoupon} className="form-control" type="text"
+              />
+            </div>
 
-          {priceFormatWithDiscount(props)}
+            {priceFormatWithDiscount(props)}
 
-          <button id="open-modal-confirm" data-toggle="modal" data-target="#confirmModal" className="btn btn-block btn-info">
-            Order
-          </button>
+            <button
+              id="open-modal-confirm"
+              data-toggle="modal" data-target="#confirmModal" className="btn btn-block btn-info"
+            >
+              Order
+            </button>
+          </div>
         </div>
       </div>
     </div>
 
-    <div id="confirmModal" className="modal fade text-center" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    {pagination(props)}
+
+    <div
+      id="confirmModal" className="modal fade text-center"
+      tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+    >
       <div className="modal-dialog">
         <div className="modal-content">
 
@@ -201,32 +270,46 @@ export const ShoppingList = (props) => (
             <p>
               <strong>
                 By confirming you're get a token to view the progress of your order.
-                <input id="user-email" className="form-control" placeholder="You get the token per email" type="email" style={{
+                <input
+                  id="user-email" className="form-control"
+                  placeholder="You get the token per email" type="email" style={{
                     textAlign: 'center',
-                  }} />
+                  }}
+                />
               </strong>
             </p>
 
-            <p style={{
+            <p
+              style={{
                 display: props.token ? 'inherit' : 'none',
-              }}>
+              }}
+            >
               Your token is: <span id="order-token">{props.token}</span>. You can now close this window.
             </p>
-
           </div>
           <div className="modal-footer">
 
-            <button type="button" className="btn btn-default" data-dismiss="modal" style={{
+            <button
+              type="button" className="btn btn-default"
+              data-dismiss="modal" style={{
                 display: props.token ? 'none' : 'inline',
-              }}>Close</button>
-            <button id="send-order-button"  type="button" style={{
+              }}
+            >
+              Close
+            </button>
+            <button
+              id="send-order-button" type="button" style={{
                 display: props.token ? 'none' : 'inline',
-              }} onClick={() => props.sendOrder(dv('#user-email'))} className="btn btn-success">Send order</button>
+              }} onClick={() => props.sendOrder(dv('#user-email'))} className="btn btn-success"
+            >
+              Send order
+            </button>
 
-            <button id="reset-view-button" className="btn btn-primary" data-dismiss="modal"
+            <button
+              id="reset-view-button" className="btn btn-primary" data-dismiss="modal"
               style={{
-                  display: props.token ? 'inline' : 'none',
-                }}
+                display: props.token ? 'inline' : 'none',
+              }}
               onClick={props.endOrdering}
             >
             Reset the view
@@ -240,6 +323,7 @@ export const ShoppingList = (props) => (
 );
 
 ShoppingList.propTypes = {
+  token: React.PropTypes.string.isRequired,
   developers: React.PropTypes.array.isRequired,
   shoppingcard: React.PropTypes.array.isRequired,
   organizations: React.PropTypes.array.isRequired,
@@ -248,6 +332,7 @@ ShoppingList.propTypes = {
   removeFromCard: React.PropTypes.func.isRequired,
   addDevFromOrg: React.PropTypes.func.isRequired,
   endOrdering: React.PropTypes.func.isRequired,
+  sendOrder: React.PropTypes.func.isRequired,
   calculatePrice: React.PropTypes.func.isRequired,
 };
 
