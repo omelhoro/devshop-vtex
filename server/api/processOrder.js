8e02dbe55ff34d7f1ae0bc588090a1426959e384
omelhoro <igor.fischer154@gmail.com>
2016-05-16 23:26:18 +0200

    asynced api

--- server/api/processOrder.js
+++ server/api/processOrder.js
@@ -1,30 +1,21 @@
 import {errorHandler} from '../lib/utils';
 const BP = require('bluebird');
-const levelup = require('levelup')
+const levelup = require('levelup');
 const uuid = require('node-uuid');
-const db = levelup(process.env.DB_PATH || './db');
+const db = BP.promisifyAll(levelup(process.env.DB_PATH || './db'));
 import {sendToken} from '../lib/emailService';
 
 async function processOrderAsync(req, res) {
   const token = uuid.v1();
-  // await BP.promisify(db.put)();
-  db.put(token, JSON.stringify({...req.body.appState, token}), (err) => {
-    console.log(err);
-    if (err) {
-      errorHandler.bind(res, err)();
-      return;
-    }
+  await db.putAsync(token, JSON.stringify({...req.body.appState, token}));
 
-    sendToken({to: req.body.email, token}, console.log);
-    res.send({token});
-  });
+  sendToken({to: req.body.email, token}, console.log);
+  res.send({token});
 }
 
 async function getOrderAsync(req, res) {
-  // const value = await BP.promisify(db.get)(req.query.token);
-  db.get(req.query.token, (err, value) => {
-    res.send(JSON.parse(value || '{}'));
-  });
+  const value = await db.getAsync(req.query.token);
+  res.send(JSON.parse(value || '{}'));
 }
 
 export const processOrder = (req, res) =>
