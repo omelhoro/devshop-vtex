b161e74aa935e1c67810f30a6694635b7b18d37f
omelhoro <igor.fischer154@gmail.com>
2016-05-18 01:17:13 +0200

    import of initial state

--- src/routes/ShoppingCard/containers/Container.js
+++ src/routes/ShoppingCard/containers/Container.js
@@ -1,5 +1,5 @@
 import { connect } from 'react-redux'
-import { increment, doubleAsync, actions } from '../modules/redux'
+import {actions, initialState} from '../modules/redux'
 
 /*  This is a container component. Notice it does not contain any JSX,
     nor does it import React. This component is **only** responsible for
@@ -34,4 +34,4 @@ const mapStateToProps = (state) => ({
     Selectors are composable. They can be used as input to other selectors.
     https://github.com/reactjs/reselect    */
 
-export default connect(mapStateToProps, mapActionCreators)(ShoppingCard)
+export default connect(mapStateToProps, mapActionCreators)(ShoppingCard);
