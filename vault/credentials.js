c29ccd6b3a6209d18c910ff2a0361b246d363589
omelhoro <igor.fischer154@gmail.com>
2016-05-18 17:50:23 +0200

    Replaced keys with encrypted keys

--- vault/credentials.js
+++
@@ -1,8 +0,0 @@
-export const github = {
-  token: 'ab59b1434c09df8f9559c2d2098620e69b1ce643',
-};
-
-export const mailgun = {
-  apiKey: 'key-881442701a3051e362f229384e73234a',
-  domain: 'sandbox36b712ee182946e48ab47f81feba2a96.mailgun.org',
-};
