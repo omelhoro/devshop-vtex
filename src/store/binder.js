2025b7cb380d5f04823697c9c5459ac8db039be3
omelhoro <igor.fischer154@gmail.com>
2016-05-18 02:59:18 +0200

    binder for not using .bind syntax anymore

---
+++ src/store/binder.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+export default function bindClosures(closuresMap) {
+  return (Component) => {
+    const componentName = Component.displayName || Component.name || 'Component';
+    const closureNames = Object.keys(closuresMap);
+    const spec = closureNames.reduce((memo, closureName) => {
+      const injectedClosure = closuresMap[closureName];
+      memo[closureName] = function boundClosure(...args) {
+        return injectedClosure(this.props, ...args);
+      };
+
+      return memo;
+    }, {});
+
+    spec.componentWillMount = function componentWillMount() {
+      this.__closures = closureNames.reduce((memo, closureName) => {
+        memo[closureName] = this[closureName];
+        return memo;
+      }, {});
+    };
+
+    spec.render = function render() {
+      const newProps = {
+        ...this.props,
+        ...this.__closures,
+      };
+
+      console.log(newProps, newProps.props);
+      return Component(newProps);
+    };
+
+    const Wrapper = React.createClass(spec);
+    Wrapper.displayName = `ClosureWrapper(${componentName})`;
+
+    return Wrapper;
+  };
+}
