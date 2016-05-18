98fd26e44731d54473a979d146ae63aeacd4e2c9
omelhoro <igor.fischer154@gmail.com>
2016-05-18 12:02:59 +0200

    active name should now work and satifsy linter

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {IndexLink, Link} from 'react-router';
 import classes from './Header.scss';
 
-import { connect } from 'react-redux'
+import {connect} from 'react-redux';
 
 export const Header = (props) => (
   <div className="text-center">
@@ -12,7 +12,14 @@ export const Header = (props) => (
     </IndexLink>
     {' · '}
     <Link to="/shoppinglist" className={classes.headerLink} activeClassName={classes.activeRoute}>
-      Shopping List <span className={classes.shoppingcardCounter} hidden={!props.shoppingcard.length}>({props.shoppingcard.length})</span>
+      Shopping List
+      {' '}
+      <span
+        className={classes.shoppingcardCounter}
+        hidden={!props.shoppingcard.length}
+      >
+        ({props.shoppingcard.length})
+      </span>
     </Link>
   </div>
 );
@@ -21,8 +28,13 @@ Header.defaultProps = {
   shoppingcard: [],
 };
 
+Header.propTypes = {
+  shoppingcard: React.PropTypes.array.isRequired,
+};
+
 const mapStateToProps = (state) => ({
-  ...state.shoppinglist,
+  ...state.router,
+  shoppingcard: state.shoppinglist.shoppingcard,
 });
 
 export default connect(mapStateToProps, {})(Header);
