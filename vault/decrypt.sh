661651decfc1756e016ac856022362bdb6c25d12
omelhoro <igor.fischer154@gmail.com>
2016-05-18 19:39:01 +0200

    Check for exit code of gpg

--- vault/decrypt.sh
+++ vault/decrypt.sh
@@ -4,8 +4,12 @@ if [ -z "$VAULT_PASS" ];
 	then
 	echo "Could not decrypt the vault without key"
 	exit 1
-fi;
+fi
 
 gpg --passphrase "$VAULT_PASS" --batch --yes vault/secret.tar.gpg
+if [ $? -ne 0 ]; then
+	echo "Something went wrong with decryption"
+fi
+
 tar -zxvf vault/secret.tar
 rm vault/secret.tar
--- vault/encrypt.sh
+++ vault/encrypt.sh
@@ -8,5 +8,10 @@ fi;
 
 tar -zcvf vault/secret.tar ./vault/secret
 gpg --symmetric --passphrase "$VAULT_PASS" vault/secret.tar
+
+if [ $? -ne 0 ]; then
+	echo "Something went wrong with encryption"
+fi
+
 rm ./vault/secret.tar
 rm -rf ./vault/secret
