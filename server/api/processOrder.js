e32b2985994d31f84f1f93959032e780483de38d
omelhoro <igor.fischer154@gmail.com>
2017-12-10 02:14:36 +0100

    use referer for email for retrieving cart

--- server/api/processOrder.js
+++ server/api/processOrder.js
@@ -1,16 +1,18 @@
-import {errorHandler} from '../lib/utils';
+import { errorHandler } from '../lib/utils';
 const BP = require('bluebird');
 const levelup = require('levelup');
+const leveldown = require('leveldown');
 const uuid = require('node-uuid');
-const db = BP.promisifyAll(levelup(process.env.DB_PATH || './db'));
-import {sendToken} from '../lib/emailService';
+const db = BP.promisifyAll(levelup(leveldown(process.env.DB_PATH || './db')));
+import { sendToken } from '../lib/emailService';
 
 async function processOrderAsync(req, res) {
   const token = uuid.v1();
-  await db.putAsync(token, JSON.stringify({...req.body.appState, token}));
-
-  sendToken({to: req.body.email, token}, console.log);
-  res.send({token});
+  await db.putAsync(token, JSON.stringify({ ...req.body.appState, token }));
+  const matchHost = /^https?:\/\/.*\//;
+  const host = matchHost.exec(req.headers.referer)[0];
+  sendToken({ to: req.body.email, host, token }, console.log);
+  res.send({ token });
 }
 
 async function getOrderAsync(req, res) {
@@ -20,8 +22,8 @@ async function getOrderAsync(req, res) {
 
 export const processOrder = (req, res) =>
   processOrderAsync(req, res)
-  .catch(errorHandler.bind(res));
+    .catch(errorHandler.bind(res));
 
 export const getOrder = (req, res) =>
   getOrderAsync(req, res)
-  .catch(errorHandler.bind(res));
+    .catch(errorHandler.bind(res));
